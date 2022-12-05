41138b13aa3dbe7bf5f57be96b92ad6e055abdb9
[  ] <blank.parenthesis@gmail.com>
2022-12-05 12:13:57 +0000

    Solve day 5

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ enum Day {
 	Two,
 	Three,
 	Four,
+	Five,
 }
 
 impl clap::ValueEnum for Day {
@@ -40,6 +41,7 @@ impl clap::ValueEnum for Day {
 			Day::Two,
 			Day::Three,
 			Day::Four,
+			Day::Five,
 		]
 	}
 
@@ -49,6 +51,7 @@ impl clap::ValueEnum for Day {
 			Day::Two => Some(PossibleValue::new("2").aliases(&["two", "2nd", "second"])),
 			Day::Three => Some(PossibleValue::new("3").aliases(&["three", "3rd", "third"])),
 			Day::Four => Some(PossibleValue::new("4").aliases(&["four", "4th", "fourth"])),
+			Day::Five => Some(PossibleValue::new("5").aliases(&["five", "5th", "fifth"])),
 		}
 	}
 }
@@ -126,6 +129,75 @@ fn main() {
 					println!("{:?}", overlapping.count())
 				},
 			}
+		},
+		(Day::Five, _) => {
+			let (arrangement, instructions) = std::str::from_utf8(&data).unwrap()
+				.split_once("\n\n").unwrap();
+
+			struct Instruction {
+				source: usize,
+				dest: usize,
+				count: usize,
+			}
+
+			let instructions = instructions.split('\n').map(|i| {
+				let (count, locations) = i[5..].split_once(" from ").unwrap();
+				let (source, dest) = locations.split_once(" to ").unwrap();
+				let count = count.parse().unwrap();
+				let source = source.parse().unwrap();
+				let dest = dest.parse().unwrap();
+				Instruction { source, dest, count }
+			});
+
+			let mut arrangement = arrangement.split('\n').rev();
+			let labels = arrangement.next().unwrap()
+				.split_whitespace()
+				.filter(|n| !n.is_empty())
+				.map(|n| n.parse::<usize>().unwrap())
+				.collect::<Vec<usize>>();
+
+			let mut stacks = (0..labels.len())
+				.map(|_| std::collections::VecDeque::<char>::new())
+				.collect::<Vec<_>>();
+
+			for line in arrangement {
+				for stack in 0..labels.len() {
+					let pos = stack * 4 + 1;
+					let c = line.as_bytes()[pos] as char;
+					if c != ' ' {
+						stacks[stack].push_back(c)
+					}
+				}
+			}
+
+
+			for Instruction { source, dest, count }  in instructions {
+				let source = labels.iter().position(|i| *i == source).unwrap();
+				let dest = labels.iter().position(|i| *i == dest).unwrap();
+
+				match args.part {
+					Part::A => {
+						for _ in 0..count {
+							let value = stacks[source].pop_back().unwrap();
+							stacks[dest].push_back(value);
+						}
+					},
+					Part::B => {
+						let mut buffer = std::collections::VecDeque::new();
+						for _ in 0..count {
+							let value = stacks[source].pop_back().unwrap();
+							buffer.push_front(value);
+						}
+						stacks[dest].append(&mut buffer);
+					},
+				}
+			}
+
+			
+			let message = stacks.iter()
+				.map(|stack| stack.back().unwrap_or(&' '))
+				.collect::<String>();
+			println!("{}", message);
 		},
 	}
 }
