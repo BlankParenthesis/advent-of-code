d44c974d4fc40791273a9a82da4e4985a1b9c375
[  ] <blank.parenthesis@gmail.com>
2022-12-12 11:09:48 +0000

    Solve day 12
    Oh, the misery…
    Well using a library makes the hard part easier.
    I definitely didn't handle the input ingest into that library well
    though.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{path::PathBuf, str::Utf8Error, ops::RangeInclusive, collections::HashSet};
+use std::{path::PathBuf, str::Utf8Error, ops::RangeInclusive, collections::{HashSet, HashMap}};
 
 use clap::{Parser, builder::PossibleValue};
-use nom::{bytes::complete::tag, sequence::{preceded, tuple}, combinator::{map_res, map}, character::complete::digit1, multi::separated_list0, Or, Parser as NomParser};
+use nom::{bytes::complete::tag, sequence::{preceded, tuple}, combinator::{map_res, map}, character::complete::digit1, multi::separated_list0, Parser as NomParser};
 
 mod day_1;
 mod day_2;
@@ -40,6 +40,7 @@ enum Day {
 	Nine,
 	Ten,
 	Eleven,
+	Twelve,
 }
 
 impl clap::ValueEnum for Day {
@@ -56,6 +57,7 @@ impl clap::ValueEnum for Day {
 			Day::Nine,
 			Day::Ten,
 			Day::Eleven,
+			Day::Twelve,
 		]
 	}
 
@@ -72,6 +74,7 @@ impl clap::ValueEnum for Day {
 			Day::Nine => Some(PossibleValue::new("9").aliases(&["nine", "9th", "ninth"])),
 			Day::Ten => Some(PossibleValue::new("10").aliases(&["ten", "10th", "tenth"])),
 			Day::Eleven => Some(PossibleValue::new("11").aliases(&["eleven", "11th", "eleventh"])),
+			Day::Twelve => Some(PossibleValue::new("12").aliases(&["twelve", "12th", "twelfth"])),
 		}
 	}
 }
@@ -656,6 +659,105 @@ fn main() {
 			let monkey_business: usize = inspections.iter().rev().take(2).product();
 
 			println!("{}", monkey_business);
+		},
+		(Day::Twelve, _) => {
+
+			enum Node {
+				Start,
+				End,
+				Walkable(i8),
+			}
+
+			impl Node {
+				fn height(&self) -> i8 {
+					match self {
+						Node::End => 'z' as i8,
+						Node::Start => 'a' as i8,
+						Node::Walkable(v) => *v,
+					}
+				}
+			}
+			
+			let map = std::str::from_utf8(&data).unwrap()
+				.split('\n')
+				.map(|line| {
+					line.chars().map(|c| {
+						match c {
+							'S' => Node::Start,
+							'E' => Node::End,
+							c => Node::Walkable(c as i8),
+						}
+					})
+					.collect()
+				})
+				.collect::<Vec<Vec<Node>>>();
+
+			let height = map.len();
+			let width = map[0].len();
+			
+			let mut graph = petgraph::Graph::<(), ()>::new();
+
+			const OFFSETS: &[(isize, isize); 4] = &[
+				(1, 0),
+				(-1, 0),
+				(0, 1),
+				(0, -1),
+			];
+
+			let ids = (0..width).into_iter().map(|x| {
+				(0..width).into_iter().map(|y| {
+					graph.add_node(())
+				}).collect()
+			}).collect::<Vec<Vec<petgraph::graph::NodeIndex>>>();
+
+			for x in 0..width {
+				for y in 0..height {
+					let node = &map[y][x];
+					let height = node.height();
+					let gid = ids[y][x];
+
+					for (ax, ay) in OFFSETS.iter().map(|(ox, oy)| (x as isize + ox, y as isize + oy)) {
+						if ax >= 0 && ay >= 0 {
+							let other = map.get(ay as usize).and_then(|v| v.get(ax as usize));
+							if let Some(other) = other {
+								if height - other.height() >= -1 {
+									let gido = ids[ay as usize][ax as usize];
+									graph.extend_with_edges(&[
+										(gid, gido),
+									])
+								}
+							}
+						}
+
+					}
+				}
+			}
+			
+			let start = map.iter().flatten().position(|m| matches!(m, Node::Start)).unwrap();
+			let start = ids[start / width][start % width];
+
+			let end = map.iter().flatten().position(|m| matches!(m, Node::End)).unwrap();
+			let end = ids[end / width][end % width];
+
+			match args.part {
+				Part::A => {
+					let path = petgraph::algo::dijkstra(&graph, start, Some(end), |_| 1);
+					println!("{:?}", path.get(&end));
+				},	
+				Part::B => {
+					graph.reverse();
+					let path = petgraph::algo::dijkstra(&graph, end, None, |_| 1);
+					let lowest = map.iter()
+						.flatten()
+						.enumerate()
+						.filter(|(_, m)| m.height() == b'a' as i8)
+						.filter_map(|(id, _)| path.get(&ids[id / width][id % width]))
+						.copied()
+						.reduce(u32::min);
+
+					println!("{:?}", lowest);
+				},
+			}
 		},
 	}
 }
