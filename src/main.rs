8c09c41839dd7930ba75cfdce129f7512c636004
[  ] <blank.parenthesis@gmail.com>
2022-12-12 03:47:28 +0000

    Solve day 11

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{path::PathBuf, str::Utf8Error, ops::RangeInclusive, collections::HashSet};
 
 use clap::{Parser, builder::PossibleValue};
-use nom::{bytes::complete::tag, sequence::{preceded, tuple}, combinator::map_res, character::complete::digit1};
+use nom::{bytes::complete::tag, sequence::{preceded, tuple}, combinator::{map_res, map}, character::complete::digit1, multi::separated_list0, Or, Parser as NomParser};
 
 mod day_1;
 mod day_2;
@@ -39,6 +39,7 @@ enum Day {
 	Eight,
 	Nine,
 	Ten,
+	Eleven,
 }
 
 impl clap::ValueEnum for Day {
@@ -54,6 +55,7 @@ impl clap::ValueEnum for Day {
 			Day::Eight,
 			Day::Nine,
 			Day::Ten,
+			Day::Eleven,
 		]
 	}
 
@@ -69,6 +71,7 @@ impl clap::ValueEnum for Day {
 			Day::Eight => Some(PossibleValue::new("8").aliases(&["eight", "8th", "eighth"])),
 			Day::Nine => Some(PossibleValue::new("9").aliases(&["nine", "9th", "ninth"])),
 			Day::Ten => Some(PossibleValue::new("10").aliases(&["ten", "10th", "tenth"])),
+			Day::Eleven => Some(PossibleValue::new("11").aliases(&["eleven", "11th", "eleventh"])),
 		}
 	}
 }
@@ -531,6 +534,128 @@ fn main() {
 			if matches![args.part, Part::A] {
 				println!("{}", strength);
 			}
+		},
+		(Day::Eleven, _) => {
+			let monkeys = std::str::from_utf8(&data).unwrap()
+				.split("\n\n");
+
+			fn take_number<Num>(input: &str) -> nom::IResult<&str, Num>
+			where Num: std::str::FromStr{
+				map_res(digit1, str::parse)(input)
+			}
+
+			fn take_numbers<Num>(input: &str) -> nom::IResult<&str, Vec<Num>>
+			where Num: std::str::FromStr{
+				separated_list0(tag(", "), map_res(digit1, str::parse))(input)
+			}
+
+			#[derive(Debug, Clone)]
+			enum Operation {
+				Add(isize),
+				Multiply(isize),
+				Squared,
+			}
+
+			impl Operation {
+				fn parse(input: &str) -> nom::IResult<&str, Self> {
+					preceded(tag("new = old "), 
+						map(preceded(tag("* "), take_number), Operation::Multiply)
+						.or(map(preceded(tag("+ "), take_number), Operation::Add))
+						.or(map(tag("+ old"), |_| Operation::Multiply(2)))
+						.or(map(tag("* old"), |_| Operation::Squared))
+					)(input)
+				}
+
+				fn perform(&self, input: isize) -> isize {
+					match self {
+						Self::Squared => input * input,
+						Self::Add(value) => input + value,
+						Self::Multiply(value) => input * value,
+					}
+				}
+			}
+
+			#[derive(Debug, Clone)]
+			struct Test {
+				number: isize,
+				value_true: usize,
+				value_false: usize,
+			}
+
+			#[derive(Debug)]
+			struct Monkey {
+				id: isize,
+				items: Vec<isize>,
+				operation: Operation,
+				test: Test,
+			}
+
+			let mut monkeys = monkeys.map(|i| {
+				let (i, (_, id, _)) =          tuple((tag("Monkey "), take_number, tag(":\n")))(i).unwrap();
+				let (i, (_, items, _)) =       tuple((tag("  Starting items: "), take_numbers, tag("\n")))(i).unwrap();
+				let (i, (_, operation, _)) =   tuple((tag("  Operation: "), Operation::parse, tag("\n")))(i).unwrap();
+				let (i, (_, test_number, _)) = tuple((tag("  Test: divisible by "), take_number, tag("\n")))(i).unwrap();
+				let (i, (_, test_true, _)) =   tuple((tag("    If true: throw to monkey "), take_number, tag("\n")))(i).unwrap();
+				let (_, (_, test_false)) =     tuple((tag("    If false: throw to monkey "), take_number))(i).unwrap();
+
+				let test = Test {
+					number: test_number,
+					value_true: test_true,
+					value_false: test_false,
+				};
+
+				Monkey { id, items, operation, test }
+			})
+			.collect::<Vec<_>>();
+
+			monkeys.sort_by(|a, b| a.id.cmp(&b.id));
+
+			let MAX_ROUNDS = match args.part {
+				Part::A => 20,
+				Part::B => 10000,
+			};
+
+			let WORRY_DECAY = match args.part {
+				Part::A => 3,
+				Part::B => 1,
+			};
+
+			let mut inspections = vec![0; monkeys.len()];
+
+			// very simple gcd
+			let gcd: isize = monkeys.iter()
+				.map(|m| m.test.number)
+				.product();
+
+			for round in 1..=MAX_ROUNDS {
+				for monkey_id in 0..monkeys.len() {
+					let monkey = monkeys.get_mut(monkey_id).unwrap();
+
+					let items = monkey.items.clone();
+					inspections[monkey_id] += items.len();
+					monkey.items = vec![];
+					let op = monkey.operation.clone();
+					let test = monkey.test.clone();
+
+
+					for old in items {
+						let new = (op.perform(old) / WORRY_DECAY) % gcd;
+
+						let pass_monkey = if new % test.number == 0 {
+							test.value_true
+						} else {
+							test.value_false
+						};
+
+						monkeys.get_mut(pass_monkey).unwrap().items.push(new);
+					}
+				}
+			}
+
+			inspections.sort();
+			let monkey_business: usize = inspections.iter().rev().take(2).product();
+
+			println!("{}", monkey_business);
 		},
 	}
 }
